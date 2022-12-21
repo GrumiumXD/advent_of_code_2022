dd33855f989964ef8bc0333a6b07524a0964dd63
Gunnar Raßmann <g_rassmann@mail.de>
2022-12-21 20:00:28 +0100

    ignored test for day 19

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -212,6 +212,7 @@ Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsid
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
 
     #[test]
+    #[ignore]
     fn p1() {
         let result = puzzle_1(INPUT);
         assert_eq!(result, "33");
